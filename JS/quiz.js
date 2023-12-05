266a8391416b90ab2ffad249a0754cbfcd54299f
dimer09 <merdingoie@gmail.com>
2023-12-05 18:42:45 +0100

    Ajout de la partie quiz

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,32 +1,98 @@
-// Vous pouvez remplacer cette variable par la récupération de la session ou une autre méthode.
-const username = "Harry Potter"; // Exemple de nom d'utilisateur
+// Simulez un ensemble de questions pour le quiz
+const questions = [
+    {
+        question: "Quelle était la principale conséquence de la signature du Statut International de Secret en 1689?",
+        answers: [
+            "La création du Ministère de la Magie",
+            "Le camouflage du monde magique aux Moldus",
+            "L'établissement d'Azkaban",
+            "La fondation de la Confédération Internationale des Sorciers"
+        ],
+        correctAnswer: 1 // Index de la réponse correcte dans le tableau 'answers'
+    },
+    {
+        question: "Qui a découvert les douze usages du sang de dragon ?",
+        answers: [
+            "Merlin",
+            "Albus Dumbledore",
+            "Nicolas Flamel",
+            "Newt Scamander"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "Quel est le nom complet de Lord Voldemort ?",
+        answers: [
+            "Tom Elvis Jedusor",
+            "Tom Marvolo Riddle",
+            "Tom Jedusor",
+            "Voldemort"
+        ],
+        correctAnswer: 1
+    }
+];
 
-document.getElementById('username').textContent = username;
-
-const answerList = document.getElementById('answer-list');
-const userScore = document.getElementById('user-score');
-const questionNumber = document.getElementById('question-number');
-
+let currentQuestionIndex = 0;
 let score = 0;
 
-answerList.addEventListener('click', function(event) {
-    if (event.target.tagName === 'LI') {
-        // Simuler une vérification de réponse correcte ou incorrecte.
-        const isCorrect = Math.random() > 0.5; // Exemple aléatoire
-        if (isCorrect) {
-            score++;
-            event.target.style.backgroundColor = 'lightgreen';
-        } else {
-            event.target.style.backgroundColor = 'salmon';
-        }
-        userScore.textContent = score;
-        
-        // Mettre à jour le numéro de la question et passer à la suivante
-        questionNumber.textContent = parseInt(questionNumber.textContent) + 1;
-        
-        // Désactiver les autres réponses après le choix
-        Array.from(answerList.children).forEach(li => {
-            li.style.pointerEvents = 'none';
+const userScore = document.getElementById('user-score');
+const questionNumber = document.getElementById('question-number');
+const questionContainer = document.querySelector('.question-container');
+
+// Affiche la question actuelle à l'utilisateur
+// ... (les autres parties du code restent les mêmes)
+
+// Fonction pour afficher la question suivante
+function showNextQuestion() {
+    // Retirer le contenu actuel de la question
+    const questionEl = document.querySelector('.question');
+    questionEl.innerHTML = '';
+
+    if (currentQuestionIndex < questions.length) {
+        const currentQuestion = questions[currentQuestionIndex];
+        const questionText = document.createElement('p');
+        questionText.textContent = 'Q. ' + currentQuestion.question;
+        questionEl.appendChild(questionText);
+
+        const answerList = document.createElement('ul');
+        answerList.classList.add('answer-list');
+        currentQuestion.answers.forEach((answer, index) => {
+            const answerItem = document.createElement('li');
+            answerItem.textContent = answer;
+            answerItem.onclick = function() { checkAnswer(index, currentQuestion.correctAnswer); };
+            answerList.appendChild(answerItem);
         });
+
+        questionEl.appendChild(answerList);
+    } else {
+        showFinalResults();
     }
-});
+}
+
+// Fonction pour vérifier la réponse et afficher la suivante
+function checkAnswer(selectedIndex, correctIndex) {
+    if (selectedIndex === correctIndex) {
+        score++;
+        userScore.textContent = score;
+    }
+    currentQuestionIndex++;
+    showNextQuestion();
+}
+
+// Fonction pour afficher les résultats finaux
+function showFinalResults() {
+    const questionContainer = document.querySelector('.question-container');
+    questionContainer.innerHTML = `
+        <div id="final-result">
+            <h1>Votre score final est : ${score} / ${questions.length}</h1>
+            ${score > 5 ? '<img id="badge" src="chemin_vers_votre_image_de_badge.png">' : ''}
+            <div class="button-container">
+                <button onclick="window.location.href='index.html';">Accueil</button>
+                <button onclick="window.location.href='cours.html';">Cours</button>
+            </div>
+        </div>
+    `;
+}
+
+// Démarrer le quiz avec la première question
+showNextQuestion();
